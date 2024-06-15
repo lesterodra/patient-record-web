429c82f87f5d0cadddf7acb5de344316330ffc9c
lesterodra <lesterodra@gmail.com>
2024-06-15 19:31:32 +0800

    added main loader

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from "react";
 import CustomAlert from "../components/CustomAlert";
 import NavBar from "../components/NavBar";
+import MainLoader from "../components/MainLoader";
 
 export default function RootLayout({
   children,
@@ -12,7 +14,7 @@ export default function RootLayout({
       <NavBar />
       <div className="p-4 sm:ml-64">
         <div className="p-0 rounded-lg dark:border-gray-700 mt-14">
-          {children}
+          <Suspense fallback={<MainLoader />}>{children}</Suspense>
         </div>
       </div>
     </div>
---
+++ src/app/components/MainLoader.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import LoadingSpinner from "./LoadingSpinner";
+
+const MainLoader = () => {
+  return (
+    <div className="center">
+      <LoadingSpinner />
+    </div>
+  );
+};
+
+export default MainLoader;
